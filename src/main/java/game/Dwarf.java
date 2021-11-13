839641185fa7660d642d72095712aaf916e4fd98
kwiatek023 <kwiatecki2000@gmail.com>
2021-11-13 20:16:47 +0100

    added Dwarf, Node and OsmService

---
+++ src/main/java/game/Dwarf.java
@@ -0,0 +1,24 @@
+package game;
+
+import osm.Node;
+
+public class Dwarf {
+    private Node node;
+    private int points;
+
+    public Node getNode() {
+        return node;
+    }
+
+    public void setNode(Node node) {
+        this.node = node;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
