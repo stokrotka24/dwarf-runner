d545b3d3df4189defa1bdc96dab67b1027b86010
CeasarArezzo <ceasararezzo@gmail.com>
2021-11-22 19:03:49 +0100

    add temporary fix to enable Login while DB is not working, remove ServerID from Message class

--- src/main/java/dbconn/UserAuthenticator.java
+++ src/main/java/dbconn/UserAuthenticator.java
@@ -27,27 +27,30 @@ public class UserAuthenticator {
             return;
         }
         
-        try {
-            CallableStatement cStatement = DBConnection.getConnection().prepareCall(loginQuery);
-            cStatement.setString(1, credentials.getEmail());
-            cStatement.setString(2, credentials.getPassword());
-            cStatement.registerOutParameter(1, java.sql.Types.VARCHAR);
-            cStatement.executeQuery();
-            String userNickname = cStatement.getString(1);
-            if (!userNickname.isEmpty()) {
-                sendSuccessResponse(userNickname, creator);
-                return;
-            }
-            else {
-                sendFailureResponse("WRONG_CREDENTIALS", creator);
-                return;
-            }
-        }
-        catch (SQLException ex) {
-            System.out.println(ex.getMessage());
-        }
-        sendFailureResponse("UNKNOWN", creator);
+        sendSuccessResponse("Przemek", creator);
         return;
+        //TODO: uncomment this when DB works again :)))
+//        try {
+//            CallableStatement cStatement = DBConnection.getConnection().prepareCall(loginQuery);
+//            cStatement.setString(1, credentials.getEmail());
+//            cStatement.setString(2, credentials.getPassword());
+//            cStatement.registerOutParameter(1, java.sql.Types.VARCHAR);
+//            cStatement.executeQuery();
+//            String userNickname = cStatement.getString(1);
+//            if (!userNickname.isEmpty()) {
+//                sendSuccessResponse(userNickname, creator);
+//                return;
+//            }
+//            else {
+//                sendFailureResponse("WRONG_CREDENTIALS", creator);
+//                return;
+//            }
+//        }
+//        catch (SQLException ex) {
+//            System.out.println(ex.getMessage());
+//        }
+//        sendFailureResponse("UNKNOWN", creator);
+//        return;
     }
 
     private static void sendFailureResponse(String failureReason, User creator) {
@@ -59,6 +62,7 @@ public class UserAuthenticator {
     private static void sendSuccessResponse(String nickname, User creator) {
         LoginResponseData responseData = LoginResponseData.successLoginData(nickname);
         Message<LoginResponseData> respMsg = new Message<>(MessageType.LOG_IN_RESPONSE, responseData);
+        System.out.println(MessageParser.toJsonString(respMsg));
         creator.sendMessage(MessageParser.toJsonString(respMsg));
     }
     
